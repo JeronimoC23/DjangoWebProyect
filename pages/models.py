f9257525e31f8eea9fc1670262ef45fb2d777c0b
JeronimoC23 <jeronimoclinaz@gmail.com>
2021-04-04 03:53:25 -0300

    Iniciando migraciones del modelo Page

--- pages/models.py
+++ pages/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-class Page (models.Model):
+class Page(models.Model):
 
     title= models.CharField(max_length=100, verbose_name="Title")
     content=models.TextField(verbose_name="Content")
