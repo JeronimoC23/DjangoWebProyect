81905bad1d20927db0b9235dd3cc497891ee2a45
JeronimoC23 <jeronimoclinaz@gmail.com>
2021-04-07 02:31:23 -0300

    Registro completado + mensaje de registro

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from .forms import RegisterForm
+from django.contrib import messages
 # Create your views here.
 
 def index(request):
@@ -14,6 +16,18 @@ def aboutus(request):
 
 def register_page(request):
 
+    register_form = RegisterForm()
+
+    if request.method == "POST":
+            register_form = RegisterForm(request.POST)
+
+            if register_form.is_valid():
+                register_form.save()
+                messages.success(request,'Success register!')
+                return redirect('home')
+
+
     return render(request, 'users/register.html',{
-        'title':'Sign in'
+        'title':'Sign in',
+        'register_form':register_form
     } )
