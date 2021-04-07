c251defab1517247d1ba8e6e9783cbd097ac588e
JeronimoC23 <jeronimoclinaz@gmail.com>
2021-04-07 00:54:33 -0300

    Editando ADMIN

--- pages/admin.py
+++ pages/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
 from .models import Page
 # Register your models here.
+#configuracion extra
+class PageAdmin(admin.ModelAdmin):
+    readonly_fields = ('created_at', 'updated_at')
+    search_fields = ('title', 'content')
+    list_filter = ('public',)
+    list_display = ('title', 'public', 'created_at')
+    ordering = ('created_at',)
 
-admin.site.register(Page)
+
+admin.site.register(Page, PageAdmin)
 
 title = "Django Web Proyect by Jeronimo Clinaz"
 subtitle = "Management Panel"
